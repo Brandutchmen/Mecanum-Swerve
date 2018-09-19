8d00f59c18098b92c819b9152cc763a8b8e022b6
Brandon Young <brandony@discjockeynews.com>
2018-09-19 09:29:49 -0500

    Updated sensitivity

--- robot.py
+++ robot.py
@@ -30,7 +30,7 @@ class MyRobot(wpilib.IterativeRobot):
     def teleopPeriodic(self):
 #        self.LeftMec.set(self.xboxController.getY(0))
  #       self.RightMec.set(-1 * self.xboxController.getY(0))
-        self.pg.set(0.075 * self.xboxController.getX(1))
-        self.robotDrive.arcadeDrive(self.xboxController.getY(0),self.xboxController.getX(0))
+        self.pg.set(0.30 * self.xboxController.getX(1))
+        self.robotDrive.arcadeDrive(self.xboxController.getY(0) / 2,self.xboxController.getX(0) / 2)
 if __name__ == "__main__":
     wpilib.run(MyRobot)
